1a4fd271588d819c433c7bfd79ec3209fc8ea4a9
emma-coronado <emma.dz.coronado@gmail.com>
2025-10-22 18:02:09 -0400

    Add todo message for eye toggle

--- scripts/eyeToggle.js
+++ scripts/eyeToggle.js
@@ -3,13 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
   
     eyeIcons.forEach(icon => {
       icon.addEventListener("click", () => {
-        if (icon.src.includes("icons/eye-open.png")) {
-          icon.src = "icons/eye-closed.png";
-          icon.closest(".list-item").dataset.status = "watched";
-        } else {
-          icon.src = "icons/eye-open.png";
-          icon.closest(".list-item").dataset.status = "to-watch";
-        }
+        // TODO: send proper "updateWatchStatus" message to service worker instead
+
+        // if (icon.src.includes("icons/eye-open.png")) {
+        //   icon.src = "icons/eye-closed.png";
+        //   icon.closest(".list-item").dataset.status = "watched";
+        // } else {
+        //   icon.src = "icons/eye-open.png";
+        //   icon.closest(".list-item").dataset.status = "to-watch";
+        // }
       });
     });
   });
