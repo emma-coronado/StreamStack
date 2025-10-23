de36e8c6f7c2c4a3b3697e51c80d98ca3cc0493b
ErrianaT <thomaserriana@ufl.edu>
2025-10-23 18:46:58 -0400

    Fixed eye toggle

--- scripts/eyeToggle.js
+++ scripts/eyeToggle.js
@@ -1,18 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const eyeIcons = document.querySelectorAll(".eye-icon");
-  
-    eyeIcons.forEach(icon => {
-      icon.addEventListener("click", () => {
-        // TODO: send proper "updateWatchStatus" message to service worker instead
+  const eyeIcons = document.querySelectorAll(".eye-icon");
 
-        // if (icon.src.includes("icons/eye-open.png")) {
-        //   icon.src = "icons/eye-closed.png";
-        //   icon.closest(".list-item").dataset.status = "watched";
-        // } else {
-        //   icon.src = "icons/eye-open.png";
-        //   icon.closest(".list-item").dataset.status = "to-watch";
-        // }
-      });
+  eyeIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+      // Toggle watched/unwatched
+      if (icon.src.includes("icons/eye-open.png")) {
+        icon.src = "icons/eye-closed.png";
+        icon.closest(".list-item").dataset.status = "watched";
+      } else {
+        icon.src = "icons/eye-open.png";
+        icon.closest(".list-item").dataset.status = "to-watch";
+      }
     });
   });
-  
+});
